cbeab3ae446991acdfb58ee2eb480e58348fe20e
ainokoo <ainokoop@hotmail.com>
2026-02-06 12:51:59 +0200

    Initial base for project

---
+++ calc.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef double Stack_entry;
+
+char get_command();
+bool do_command(char command, Stack &numbers);
---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include "calc.h"
+
+using namespace std;
+
+int main()
+/*
+Post: The program has executed simple arithmetic
+      commands entered by the user.
+Uses: The class Stack and the functions
+      introduction, instructions, do_command, and get_command.
+*/
+
+{
+   Stack stored_numbers;
+   introduction();
+   instructions();
+   while (do_command(get_command(), stored_numbers));
+}
+
+
